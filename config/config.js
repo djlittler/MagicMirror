cc215b195a69bd67b1bb58bc6ec11265d203a576
djlittler <daniel.littler@hotmail.com>
2020-05-26 12:53:00 +0100

    Update config.js
    Atom Reformat

--- config/config.js
+++ config/config.js
@@ -35,6 +35,84 @@ var config = {
 			     // true, force serveronly mode, because you want to.. no UI on this device
 
 	modules: [
+    {
+      module: 'MMM-Face-Reco-DNN',
+      config: {
+        // Logout 15 seconds after user was not detected any more
+        // If they are detected within this period, the delay will start again
+        logoutDelay: 15000,
+        // How often the recognition starts in milliseconds
+        // With a Raspberry Pi 3+ it works well every 2 seconds
+        checkInterval: 2000,
+        // Module set used for strangers or if no user is detected
+        defaultClass: 'default',
+        // Set of modules which should be shown for every recognised user
+        everyoneClass: 'shared',
+        // XML to recognize with haarcascade
+        cascade: 'modules/MMM-Face-Reco-DNN/tools/haarcascade_frontalface_default.xml',
+        // Pre-encoded pickle with the faces
+        encodings: 'modules/MMM-Face-Reco-DNN/tools/encodings.pickle',
+        // Use Raspberry Pi camera or another type
+        // 1 = RasPi camera, 0 = other camera
+        usePiCamera: 1,
+        // Method of facial recognition
+        // dnn = deep neural network, haar = haarcascade
+        method: 'dnn',
+        // Which face detection model to use
+        // "hog" is less accurate but faster on CPUs
+        // "cnn" is a more accurate deep-learning model which is GPU/CUDA accelerated
+        detectionMethod: 'hog',
+        // How long in milliseconds modules take to hide and show
+        animationSpeed: 0,
+        // Path to Python to run the face recognition
+        // null or '' means default path
+        pythonPath: null,
+        // Should a welcome message be shown using the MagicMirror alerts module?
+        welcomeMessage: true,
+        // Capture new pictures of recognized people, if unknown we save it in folder "unknown"
+        // So you can extend your dataset and retrain it afterwards for better recognitions
+        extendDataset: false,
+        // If extendDataset is true, you need to set the full path of the dataset
+        dataset: 'modules/MMM-Face-Reco-DNN/dataset/'
+      }
+    },
+  	{
+      module: 'MMM-pages',
+      classes: 'default Daniel Cret Oscar Woo_Young',
+      config: {
+        modules: [
+					["MMM-Pollen", "newsfeed", "calendar", "on -this-day", "currentweather"],
+					["weatherforecast"],
+          ["compliments"]],
+        fixed: ["MMM-OnScreenMenu","clock", "currentweather", "MMM-page-indicator", "mm-hide-all", "MMM-NetworkConnection"],
+      }
+    },
+    {
+      module: 'MMM-page-indicator',
+      position: 'bottom_bar',
+      classes: 'Default Daniel Cret Oscar Woo_Young',
+      config: {
+          pages: 4,
+      }
+    },
+    {
+	    module: 'MMM-Cursor',
+	    classes: "default Daniel Cret Oscar Woo_Young",
+	    config: {
+		// See 'Configuration options' for more information.
+	    }
+    },
+		{
+      module: 'MMM-OnScreenMenu',
+      position: 'bottom_right',
+      classes: 'Daniel',
+      /* Valid positions: 'top_right', 'top_left', 'bottom_right', 'bottom_left' */
+      config: {
+        touchMode: true,
+        enableKeyboard: true,
+        // ... see more options below
+      }
+    },
 		{
 			module: "alert",
 			classes: "default Daniel Cret Oscar Woo_Young"
@@ -55,10 +133,11 @@ var config = {
 			position: "top_left",
 			classes: 'Daniel Cret Oscar Woo_Young',
 			config: {
-				calendars: [
+				calendars:[
 					{
 						symbol: "calendar-check",
-						url: "webcal://www.calendarlabs.com/ical-calendar/ics/76/US_Holidays.ics"					}
+						url: "webcal://www.calendarlabs.com/ical-calendar/ics/76/US_Holidays.ics"
+					}
 				]
 			}
 		},
@@ -105,116 +184,32 @@ var config = {
 				broadcastNewsUpdates: true
 			}
 		},
-		{
-            module: 'MMM-OnScreenMenu',
-            position: 'bottom_right',
-            classes: 'Daniel',
-            /* Valid positions: 'top_right', 'top_left', 'bottom_right', 'bottom_left' */
-            config: {
-                touchMode: true,
-                enableKeyboard: true,
-                // ... see more options below
-            }
-        },
-        {
-            module: 'MMM-NetworkConnection',
-            position: 'top_right',
-            classes: 'Daniel',
-            config: {
-            }
-        },
-        {
-            module: 'on-this-day',
-            position: 'bottom_bar',
-            classes: "Daniel Cret Oscar Woo_Young",
-            config: {
-                // see below for configurable options...
-            }
-        },
-       {
-            module: 'MMM-pages',
-            classes: 'default Daniel Cret Oscar Woo_Young',
-            config: {
-                modules:
-                    [["MMM-Pollen", "newsfeed", "calendar", "on -this-day", "currentweather"],
-                     ["weatherforecast"],
-                     ["compliments"]],
-                fixed: ["MMM-OnScreenMenu","clock", "currentweather", "MMM-page-indicator", "mm-hide-all", "MMM-NetworkConnection"],
-            }
-        },
-        {
-            module: 'MMM-page-indicator',
-            position: 'bottom_bar',
-            classes: 'Default Daniel Cret Oscar Woo_Young',
-            config: {
-                pages: 4,
-            }
-        },
-        {
-		    module: 'MMM-Cursor',
-		    classes: "default Daniel Cret Oscar Woo_Young",
-		    config: {
-			// See 'Configuration options' for more information.
-		    }
-	    },
-	    {
-            module: "MMM-Pollen",
-            position: "top_left",
-            header: "Pollen Forecast",
-            classes: 'Daniel Cret',
-            config: {
-                updateInterval: 3 * 60 * 60 * 1000, // every 3 hours
-                zip_code: "90210"
-            }
-        },
-        {
-	        module: 'mm-hide-all',
-	        position: 'bottom_right',
-	        classes: "default Daniel Cret Oscar Woo_Young"
-        },
-        {
-            module: 'MMM-Face-Reco-DNN',
-            config: {
-                // Logout 15 seconds after user was not detected any more
-                // If they are detected within this period, the delay will start again
-                logoutDelay: 15000,
-                // How often the recognition starts in milliseconds
-                // With a Raspberry Pi 3+ it works well every 2 seconds
-                checkInterval: 2000,
-                // Module set used for strangers or if no user is detected
-                defaultClass: 'default',
-                // Set of modules which should be shown for every recognised user
-                everyoneClass: 'shared',
-                // XML to recognize with haarcascade
-                cascade: 'modules/MMM-Face-Reco-DNN/tools/haarcascade_frontalface_default.xml',
-                // Pre-encoded pickle with the faces
-                encodings: 'modules/MMM-Face-Reco-DNN/tools/encodings.pickle',
-                // Use Raspberry Pi camera or another type
-                // 1 = RasPi camera, 0 = other camera
-                usePiCamera: 1,
-                // Method of facial recognition
-                // dnn = deep neural network, haar = haarcascade
-                method: 'dnn',
-                // Which face detection model to use
-                // "hog" is less accurate but faster on CPUs
-                // "cnn" is a more accurate deep-learning model which is GPU/CUDA accelerated
-                detectionMethod: 'hog',
-                // How long in milliseconds modules take to hide and show
-                animationSpeed: 0,
-                // Path to Python to run the face recognition
-                // null or '' means default path
-                pythonPath: null,
-                // Should a welcome message be shown using the MagicMirror alerts module?
-                welcomeMessage: true,
-                // Capture new pictures of recognized people, if unknown we save it in folder "unknown"
-                // So you can extend your dataset and retrain it afterwards for better recognitions
-                extendDataset: false,
-                // If extendDataset is true, you need to set the full path of the dataset
-                dataset: 'modules/MMM-Face-Reco-DNN/dataset/'
-            }
-        },
+    {
+      module: 'MMM-NetworkConnection',
+      position: 'top_right',
+      classes: 'Daniel',
+      config: {
+      }
+    },
+    {
+      module: 'on-this-day',
+      position: 'bottom_bar',
+      classes: "Daniel Cret Oscar Woo_Young",
+      config: {
+        // see below for configurable options...
+      }
+    },
+  	{
+      module: "MMM-Pollen",
+      position: "top_left",
+      header: "Pollen Forecast",
+      classes: 'Daniel Cret',
+      config: {
+        updateInterval: 3 * 60 * 60 * 1000, // every 3 hours
+        zip_code: "90210"
+      }
+    },
 	]
-
 };
 
 /*************** DO NOT EDIT THE LINE BELOW ***************/

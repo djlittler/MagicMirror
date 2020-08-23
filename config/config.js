26928bda0767aa4519077dda5b917f7987c8be45
djlittler <daniel.littler@hotmail.com>
2020-08-23 16:11:36 +0100

    Clocks

--- config/config.js
+++ config/config.js
@@ -41,17 +41,17 @@ var config = {
       config: {
         modules: [
 					//Page 1: Homepage
-					["clock", "MMM-Screencast", "MMM-Spotify", "MMM-network-signal"],
+					["clock", "MMM-Screencast", "MMM-OClock", "MMM-Spotify", "MMM-network-signal"],
 					//Page 2: Daily Brief
-					["currentweather", "weatherforecast", "MMM-Pollen", "MMM-AVStock", "MMM-CoinMarketCap", "newsfeed"],
+					["MMM-DigClock", "currentweather", "weatherforecast", "MMM-Pollen", "MMM-AVStock", "MMM-CoinMarketCap", "newsfeed"],
 					//Page 3: Scheduler
-          ["calendar", "holidays", "MMM-HabiticaStats", "MMM-Todoist", "MMM-CountDOWN", "MMM-CountUP"],
+          ["MMM-DigClock", "calendar", "holidays", "MMM-HabiticaStats", "MMM-Todoist", "MMM-CountDOWN", "MMM-CountUP"],
 					//Page 4: For You
-					["MMM-ISS", "MMM-ISS-Live", "on-this-day", "compliments"],
+					["MMM-DigClock", "MMM-ISS", "MMM-ISS-Live", "on-this-day", "compliments"],
 					//Page 5: Work/Dump
-					["MMM-Tube-Status"],
+					["MMM-DigClock", "MMM-Tube-Status"],
 					//Page 6: Settings
-					["MMM-SendNotificationButton", "MMM-NetworkConnection"]],
+					["MMM-DigClock", "MMM-SendNotificationButton", "MMM-NetworkConnection"]],
         fixed: ["MMM-OnScreenMenu", "MMM-page-indicator", "mm-hide-all", "MMM-Face-Reco-DNN", "MMM-Modulebar", "MMM-ViewNotifications"],
       }
 			//"MMM-SimpleLogo"
@@ -161,6 +161,17 @@ var config = {
 			position: "top_left",
 			classes: 'default Daniel Cret Oscar Woo_Young'
 		},
+		{
+		module: "MMM-DigClock",
+		position: "top_bar",	// This can be any of the regions.
+		config: {
+			showDate: true,
+			showWeek: false,
+			showSeconds: false,
+			dateFormat: "ddd, ll",
+			timezone: "America/Chicago"
+			}
+		},
 
 
 
@@ -197,6 +208,65 @@ var config = {
 		      scroll: true, // scroll title / artist / album in miniBar style
 		      logo: true, // display Spotify logo in miniBar style
 		    }
+		  }
+		},
+		{
+		  module: "MMM-OClock",
+		  position: "middle_center",
+		  config: {
+		    locale: "", //default is system locale, or use like this. "de-DE"
+		    canvasWidth:1000,
+		    canvasHeight:1000,
+		    centerColor: "#FFFFFF",
+		    centerR: 50,
+		    centerTextFormat: "YYYY",
+		    centerFont: "bold 20px Roboto",
+		    centerTextColor:"#000000",
+		    hands: ["month", "date", "day", "hour", "minute", "second"],
+		    //available values; "year", "month", "date", "week", "day", "hour", "minute", "second"
+		    handType: "round", //"default", "round"
+		    handWidth: [40, 40, 40, 40, 40, 40],
+		    handTextFormat: ["MMM", "Do", "ddd", "h", "m", "s"],
+		    handFont: "bold 16px Roboto",
+		    useNail: true,
+		    nailSize: 40,
+		    nailBgColor: "#000000",
+		    nailTextColor: "#FFFFFF", //CSS color or "inherit"
+		    space: 3,
+		    colorType: "hsv", //availables: "static", "radiation", "transform", "hsv"
+		    colorTypeStatic: ["red", "orange", "yellow", "green", "blue", "purple"],
+		    colorTypeRadiation: ["#333333", "red"],
+		    colorTypeTransform: ["blue", "red"],
+		    colorTypeHSV: 0.25, //hsv circle start color : 0~1
+
+		    handConversionMap: { // I think you don't need to modify this.
+		      "year": "YYYY",
+		      "month": "M",
+		      "date": "D",
+		      "week": "w", // Local week of year. If you want to use ISO week of year, use "W" instead "w"
+		      "day": "e", // Local day of week. If you want to use ISO day of week, use "E" instead "e"
+		      "hour": "h", // 12H system. If you want to 24H system, use "H" instead "h"
+		      "minute": "m",
+		      "second": "s"
+		    },
+
+		    ////// Available as of v2.x:
+		    secondsUpdateInterval: 1,  // how often to redraw the seconds hand (integer >= 1 sec)
+
+		    // To show an age bar enter your birthYear AND enter "age" in the hands array.
+		    birthYear: false,  // e.g. 1901
+		    birthMonth: 0,    // e.g. 1-12 (optional, recommended)
+		    lifeExpectancy: 85, // default: 85
+		    linearLife: false,  // set to true to plot life linearly not logarithmically
+		                        // Inspiration: http://www.bertrandplanes.com/pages/LifeClock3.php
+		    ageBarColor: [],  // false for no gradient, empty array for default, or
+		                      // [start, stop] colors, e.g. ['#000', 'white']
+
+		    scale: 1, // convenience to scale bar dimensions (font size & nailSize should be
+		              // adjusted manually).  E.g. 0.5 is half-size, 2 is double sized.
+
+		    canvasStyle: "", // Any CSS styles, e.g. "opacity: .7; ..."
+
 		  }
 		},
 		{
@@ -443,7 +513,7 @@ var config = {
 		//Module: On This Day
     {
       module: 'on-this-day',
-      position: 'bottom_bar',
+      position: 'bottom_center',
       classes: "default Daniel Cret Oscar Woo_Young day_interface",
       config: {
         // see below for configurable options...
